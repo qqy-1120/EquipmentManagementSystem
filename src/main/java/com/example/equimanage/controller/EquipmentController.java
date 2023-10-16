ba724780a1b2474963db6fc5ea3018ba81f55d57
qingyang <951953812@qq.com>
2023-10-16 18:42:12 +0800

    image upload

--- src/main/java/com/example/equimanage/controller/EquipmentController.java
+++ src/main/java/com/example/equimanage/controller/EquipmentController.java
@@ -13,7 +13,6 @@ import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 
 @RestController
